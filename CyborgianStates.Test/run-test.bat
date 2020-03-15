da8b91fa657219ae086a9fb78bac3f563bbe8768
drehtisch <38186302+drehtisch@users.noreply.github.com>
2020-03-15 23:55:14 +0100

    Fix tests

--- CyborgianStates.Test/run-test.bat
+++ CyborgianStates.Test/run-test.bat
@@ -1,9 +1,7 @@
 @echo off
-cd ..\CyborgianStates
-dotnet clean CyborgianStates.sln
-dotnet build CyborgianStates.sln
-cd ..\CyborgianStates.Test
-coverlet .\bin\Debug\netcoreapp3.1\CyborgianStates.dll --target "dotnet" --targetargs "test --no-build" --exclude-by-file "%UserProfile%\.nuget\packages\microsoft.net.test.sdk\16.4.0\build\netcoreapp2.1\Microsoft.NET.Test.Sdk.Program.cs" --exclude "[*]CyborgianStates.MessageHandling.ConsoleInput" --exclude "[*]CyborgianStates.AppSettings" --exclude "[*]CyborgianStates.Models.*" --format cobertura --output "./TestResults//"
+dotnet clean ..\CyborgianStates\CyborgianStates.sln
+dotnet build ..\CyborgianStates\CyborgianStates.sln
+coverlet bin\Debug\netcoreapp3.1\CyborgianStates.Test.dll --target "dotnet" --targetargs "test --no-build" --exclude "[*]CyborgianStates.MessageHandling.ConsoleInput" --exclude "[*]CyborgianStates.AppSettings" --exclude "[*]CyborgianStates.Models.*" --format cobertura --output "./TestResults//"
 pause
 reportgenerator -reports:TestResults/coverage.cobertura.xml -targetdir:TestResults/Reports -reporttypes:HtmlInline
 start TestResults/Reports/index.htm
