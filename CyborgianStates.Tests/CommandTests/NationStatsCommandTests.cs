8022ed6c7c324bbf7b377a6237d56b235636bea1
drehtisch <38186302+drehtisch@users.noreply.github.com>
2021-02-17 00:44:04 +0100

    Platform independent line breaks in assertions

--- CyborgianStates.Tests/CommandTests/NationStatsCommandTests.cs
+++ CyborgianStates.Tests/CommandTests/NationStatsCommandTests.cs
@@ -27,7 +27,7 @@ namespace CyborgianStates.Tests
             var message = new Message(0, "nation", new ConsoleMessageChannel());
             var response = await new NationStatsCommand().Execute(message);
             response.Status.Should().Be(CommandStatus.Error);
-            response.Content.Should().StartWith("Something went wrong\r\n\r\nNo parameter passed.");
+            response.Content.Should().StartWith($"Something went wrong{Environment.NewLine}{Environment.NewLine}No parameter passed.");
         }
         [Fact]
         public async Task TestCancel()
@@ -41,7 +41,7 @@ namespace CyborgianStates.Tests
             TestRequestDispatcher.PrepareNextRequest(Enums.RequestStatus.Canceled);
             var response = await command.Execute(message);
             response.Status.Should().Be(CommandStatus.Error);
-            response.Content.Should().StartWith("Something went wrong\r\n\r\nRequest/Command has been canceled. Sorry :(");
+            response.Content.Should().StartWith($"Something went wrong{Environment.NewLine}{Environment.NewLine}Request/Command has been canceled. Sorry :(");
         }
 
         [Fact]
@@ -54,11 +54,11 @@ namespace CyborgianStates.Tests
             TestRequestDispatcher.PrepareNextRequest(Enums.RequestStatus.Failed, exception: new HttpRequestFailedException(), failReason: "Request failed: 404 - NotFound");
             var response = await command.Execute(message);
             response.Status.Should().Be(CommandStatus.Error);
-            response.Content.Should().StartWith("Something went wrong\r\n\r\nRequest failed: 404 - NotFound");
+            response.Content.Should().StartWith($"Something went wrong{Environment.NewLine}{Environment.NewLine}Request failed: 404 - NotFound");
             // InvalidOperation
             response = await command.Execute(message);
             response.Status.Should().Be(CommandStatus.Error);
-            response.Content.Should().StartWith("Something went wrong\r\n\r\nAn unexpected error occured. Please contact the bot administrator.");
+            response.Content.Should().StartWith($"Something went wrong{Environment.NewLine}{Environment.NewLine}An unexpected error occured. Please contact the bot administrator.");
         }
 
         [Fact]
@@ -73,7 +73,7 @@ namespace CyborgianStates.Tests
             TestRequestDispatcher.PrepareNextRequest(response: xmlResult);
             var response = await command.Execute(message);
             response.Status.Should().Be(CommandStatus.Success);
-            response.Content.Should().StartWith("BasicStats for Nation\r\n\r\n**[The дСвобода Мысл of Testlandia](https://www.nationstates.net/nation=testlandia)**\r\n38.041 billion Testlandians | Founded 0 | Last active 5 days ago\r\n\r\nRegion                                                            Residency \r\n[Testregionia](https://www.nationstates.net/region=testregionia)   95 days  \r\n\r\nNew York Times Democracy\r\nC: Excellent (68.90) | E: Very Strong (80.00) | P: Superb (76.29)\r\n\r\nWA Member\r\nGA Vote: UNDECIDED | SC Vote: UNDECIDED |  0.00 endorsements | 6102.00 Influence (Eminence Grise)");
+            response.Content.Should().StartWith($"BasicStats for Nation{Environment.NewLine}{Environment.NewLine}**[The дСвобода Мысл of Testlandia](https://www.nationstates.net/nation=testlandia)**{Environment.NewLine}38.041 billion Testlandians | Founded 0 | Last active 5 days ago{Environment.NewLine}{Environment.NewLine}Region                                                            Residency {Environment.NewLine}[Testregionia](https://www.nationstates.net/region=testregionia)   95 days  {Environment.NewLine}{Environment.NewLine}New York Times Democracy{Environment.NewLine}C: Excellent (68.90) | E: Very Strong (80.00) | P: Superb (76.29){Environment.NewLine}{Environment.NewLine}WA Member{Environment.NewLine}GA Vote: UNDECIDED | SC Vote: UNDECIDED |  0.00 endorsements | 6102.00 Influence (Eminence Grise)");
         }
 
         private void ConfigureServices()
